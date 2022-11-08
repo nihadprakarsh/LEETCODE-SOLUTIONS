c57df7a812a22ef321f7bcfc0118ed18cfb1e609
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-11-09 02:53:35 +0530

    Time: 66 ms (12.40%), Space: 13.7 MB (70.17%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canFinish(int N, vector<vector<int>>& p) {
+        vector<int> adj[N];
+        int indegree[N];
+        memset(indegree, 0, sizeof(indegree));
+        for(auto it : p){
+            adj[it[1]].push_back(it[0]);
+        }
+        
+        for(int i =0; i < N; ++i){
+            for(auto node : adj[i]){
+                indegree[node]++;
+            }
+        }
+        
+        queue<int> q;
+        for(int i=0; i < N; ++i){
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        vector<int> topo;
+        while(!q.empty()){
+            auto node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto nodes : adj[node]){
+                indegree[nodes]--;
+                if(indegree[nodes] == 0)
+                    q.push(nodes);
+            }
+        }
+        
+        return topo.size() == N;
+    }
+};
