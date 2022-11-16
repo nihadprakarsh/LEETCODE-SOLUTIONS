e19fe233f54b3b1f8d48139fe13f1145176fdf3b
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-11-16 20:04:54 +0530

    Time: 132 ms (61.71%), Space: 14.1 MB (71.74%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-     int minCost(vector<vector<int>>& grid) {
-    int n = grid.size();
-    int m = grid[0].size();
-    vector<vector<int>> visi(n,vector<int> (m));
-    vector<vector<int>> dis(n,vector<int> (m,INT_MAX));
-    int moves[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
-    dis[0][0] = 0;
-    
-    priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > pq;
-    pq.push({0,{0,0}});
-    visi[0][0] = 1;
-    while(pq.size())
-    {
-        pair<int,pair<int,int>> mini = pq.top();
-        int r = mini.second.first;
-        int c= mini.second.second;
-        pq.pop();
-        visi[r][c] = 1;
-        for(int k=0;k<4;k++)
+    typedef pair<int,pair<int,int>> pt;
+    int minCost(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> dist(n,vector<int>(m,INT_MAX));
+        vector<vector<int>> vist(n,vector<int>(m,0));
+        dist[0][0] = 0;
+        vist[0][0] = 1;
+        priority_queue<pt,vector<pt>,greater<pt>> q;
+        int moves[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
+        q.push({0,{0,0}});
+        while(!q.empty())
         {
-            int nr = r + moves[k][0];
-            int nc = c + moves[k][1];
-            if(nr<n&&nc<m&&nr>=0&&nc>=0&&!visi[nr][nc])
+            int steps = q.top().first;
+            int r = q.top().second.first;
+            int c = q.top().second.second;
+            vist[r][c] = 1;
+            q.pop();
+            
+//             vis[grid[r][c]-1] = 1;
+            for(int k =0 ; k < 4; ++k)
             {
-                int cost = (grid[r][c]!=k+1);
-                if(dis[nr][nc]>(dis[r][c]+cost))
-                {
-                    dis[nr][nc] = dis[r][c] + cost;
-                    pq.push({dis[nr][nc],{nr,nc}});
-                }
+                int nr = r + moves[k][0];
+                int nc = c + moves[k][1];
+                if(nr<n&&nc<m&&nr>=0&&nc>=0&&!vist[nr][nc])
+                            {
+                                int cost = (grid[r][c]!=k+1);
+                                if(dist[nr][nc]>(dist[r][c]+cost))
+                                {
+                                    dist[nr][nc] = dist[r][c] + cost;
+                                    q.push({dist[nr][nc],{nr,nc}});
+                                }
+                            }
             }
-        } 
-    }
-    return dis[n-1][m-1];
+        }
+        
+        return dist[n-1][m-1];
     }
 };
