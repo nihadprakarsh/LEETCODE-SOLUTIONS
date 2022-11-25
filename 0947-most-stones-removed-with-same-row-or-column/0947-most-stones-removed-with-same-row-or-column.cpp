ad3d72c5b40d0cdccabaa8ae42e48d76d7774212
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-11-25 22:25:18 +0530

    Time: 130 ms (71.42%), Space: 36.7 MB (5.10%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,63 @@
+class DSU{
+	public:
+	vector<int> rank, parent;
+	DSU(int n){
+		rank.resize(n+1,0);
+		parent.resize(n+1);
+		for(int i=0; i <= n; ++i)
+		parent[i] = i;
+	}
+
+	int findParent(int node)
+	{
+		if(node==parent[node])
+		return node;
+		return parent[node] = findParent(parent[node]);
+	}
+
+	void Union(int u, int v)
+	{
+		int ulp_u = findParent(u);
+		int ulp_v = findParent(v);
+		if(ulp_u == ulp_v) return;
+		if(rank[ulp_u] < rank[ulp_v])
+		parent[ulp_u] = ulp_v;
+		else if(rank[ulp_v] < rank[ulp_u])
+		parent[ulp_v] = ulp_u;
+		else
+		{
+			parent[ulp_v] = ulp_u;
+			rank[ulp_u]++;
+		}
+	}
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int maxRow, maxCol;
+        unordered_map<int,int> m;
+        for(auto it : stones)
+        {
+            maxRow = max(maxRow, it[0]);
+            maxCol = max(maxCol, it[1]);
+        }
+        DSU dsu(maxRow + maxCol + 1);
+        for(auto it : stones)
+        {
+            int row = it[0];
+            int col = it[1] + maxRow + 1;
+            dsu.Union(row, col);
+            
+            m[row] = 1;
+            m[col] = 1;
+        }
+        
+        int ans = 0;
+        for(auto it : m)
+        {
+            if(dsu.findParent(it.first) == it.first)
+            ans++;
+        }
+        return stones.size()-ans;
+    }
+};
