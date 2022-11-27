77f53bc88b2498f04506afc8799b19401b4b100b
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-11-28 01:38:47 +0530

    Time: 15 ms (40.04%), Space: 11.4 MB (38.90%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,94 @@
+class DSU{
+	public:
+	vector<int> rank, parent, size;
+	DSU(int n){
+		rank.resize(n+1,0);
+		parent.resize(n+1);
+		size.resize(n+1);
+		for(int i=0; i <= n; ++i){
+		parent[i] = i;
+		size[i] = 1;
+	   }
+	}
+
+	int findParent(int node)
+	{
+		if(node==parent[node])
+		return node;
+		return parent[node] = findParent(parent[node]);
+	}
+
+	void UnionbyRank(int u, int v)
+	{
+		int ulp_u = findParent(u);
+		int ulp_v = findParent(v);
+		if(ulp_u == ulp_v) return;
+		if(rank[ulp_u] < rank[ulp_v])
+		parent[ulp_u] = ulp_v;
+		else if(rank[ulp_v] < rank[ulp_u])
+		parent[ulp_v] = ulp_u;
+		else
+		{
+			parent[ulp_v] = ulp_u;
+			rank[ulp_u]++;
+		}
+	}
+	
+	void UnionbySize(int u, int v)
+	{
+		int ulp_u = findParent(u);
+		int ulp_v = findParent(v);
+		if(ulp_u == ulp_v) return;
+		if(size[ulp_u] < size[ulp_v])
+		{
+			size[ulp_v] += size[ulp_u];
+			parent[ulp_u] = ulp_v;
+		}
+		else
+		{
+			size[ulp_u] += size[ulp_v];
+			parent[ulp_v] = ulp_u;
+		}
+	}
+};
+class Solution {
+public:
+    bool equationsPossible(vector<string>& equations) {
+        DSU dsu(27);
+        for(int i=0; i < equations.size(); ++i)
+        {
+            if(equations[i][1] == equations[i][2])
+            dsu.UnionbyRank(equations[i][0]-'a', equations[i][3]-'a');
+        }
+        for(int i=0; i < equations.size(); ++i)
+        {
+            if(equations[i][1] != equations[i][2])
+            {
+                if(dsu.findParent(equations[i][0]-'a') == dsu.findParent(equations[i][3]-'a'))
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
