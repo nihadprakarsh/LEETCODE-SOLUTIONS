6421b7c8cfbdfa1e28086b9c834277f6d2154c06
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-12-09 03:50:32 +0530

    Time: 2672 ms (5.05%), Space: 436.7 MB (5.20%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -7,15 +7,15 @@ public:
         return a.second < b.second;
     }
     set<int> st;
-    int getPow(int i,int steps)
+    int getPow(int i)
     {
         if(i==1)
         return 0;
         if(st.count(i)) -1e8;
         st.insert(i);
         int a= 0,b=0;
-        if(i%2) a = 1 + getPow(3*i+1,steps+1);
-        else b= 1 + getPow(i/2,steps+1);
+        if(i%2) a = 1 + getPow(3*i+1);
+        else b= 1 + getPow(i/2);
         return max(a,b);
     }
     int getKth(int lo, int hi, int k) {
@@ -23,7 +23,7 @@ public:
         for(int i= lo; i <= hi; ++i)
         {
             st.clear();
-            v.push_back({i,getPow(i,0)});
+            v.push_back({i,getPow(i)});
         }
         
         sort(begin(v), end(v), comp);
