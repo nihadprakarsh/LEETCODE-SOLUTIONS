d3ae5bee3af6092c76792c2bc88ffc091dae30d7
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-12-13 20:58:19 +0530

    Time: 262 ms (46.94%), Space: 16.7 MB (58.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int dp[10005];
-    int find(vector<int>& nums, int ind)
-    {
-        if(ind == nums.size()-1) return 0;
-        if(dp[ind] != -1) return dp[ind];
-        long long int ans = INT_MAX;
-        for(int i = 1; i <= nums[ind]; ++i)
-        {
-            if(i+ind <= nums.size()-1)
-            ans = min(ans+0LL,1LL + find(nums,ind+i));
-        }
-        return dp[ind] = ans;
-    }
     int jump(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return find(nums,0);
+        memset(dp, 0, sizeof(dp));
+        for(int ind = nums.size()-2; ind>=0; --ind)
+        {
+            long long int ans = INT_MAX;
+            for(int i = 1; i <= nums[ind]; ++i)
+            {
+                if(i+ind <= nums.size()-1)
+                ans = min(ans+0LL,1LL + dp[ind+i]);
+            }
+            dp[ind] = ans;
+        }
+        return dp[0];
     }
 };
