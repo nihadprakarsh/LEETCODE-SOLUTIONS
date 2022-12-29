d2e1db866ee263a26cff12f9798ee21924f0fe9a
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2022-12-29 19:35:29 +0530

    Time: 1946 ms (9.11%), Space: 6 MB (36.17%) - LeetHub

---
+++ 2002-maximum-product-of-the-length-of-two-palindromic-subsequences/2002-maximum-product-of-the-length-of-two-palindromic-subsequences.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int ans= 0;
+    bool isPallin(string &s)
+    {
+        int i =0, j=  s.size()-1;
+        while(i<j)
+        {
+            if(s[i] != s[j])
+            return false;
+            i++;j--;
+        }
+        return true;
+    }
+    void dfs(string &s, int ind, string s1, string s2)
+    {
+        if(ind==s.size())
+        {
+            if(isPallin(s1) && isPallin(s2)){
+            int tot = s1.size() * s2.size();
+            ans=max(ans,tot);
+            }
+            return;
+        }
+        
+        s1.push_back(s[ind]);
+        dfs(s,ind+1,s1,s2);
+        s1.pop_back();
+        
+        s2.push_back(s[ind]);
+        dfs(s,ind+1,s1,s2);
+        s2.pop_back();
+        
+        dfs(s,ind+1,s1,s2);
+    }
+    int maxProduct(string s) {
+        dfs(s,0,"","");
+        return ans;
+    }
+};
