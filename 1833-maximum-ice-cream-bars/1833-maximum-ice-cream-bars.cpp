4dc2762886fe7ad2a52f08a7154ec96481e691f4
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2023-01-06 13:03:36 +0530

    Time: 210 ms (69.08%), Space: 76.5 MB (32.82%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
+    bool check(int mid, vector<int>&costs, int coins)
+    {
+        int a=0;
+        for(auto &it:costs)
+        {
+            coins-=it;
+            if(coins<0) break;
+            a++;
+        }
+        return a>=mid;
+    }
     int maxIceCream(vector<int>& costs, int coins) {
         sort(begin(costs),end(costs));
-        int c=0,ans=0;
-        for(int i=0; i < costs.size(); ++i)
+        int low=0,high=INT_MAX,ans=0,mid;
+        while(low<=high)
         {
-            c+=costs[i];
-            if(c>coins)
-            return ans;
-            ans++;
+            mid=(low+high)>>1;
+            if(check(mid,costs,coins))
+            {
+                ans=mid;
+                low=mid+1;
+            }
+            else
+            high=mid-1;
         }
         return ans;
     }
