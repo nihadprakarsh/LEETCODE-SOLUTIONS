50aede41289f5e84e5e1424735eaf754ee74ae4e
Nihad Prakarsh <81522007+nihadprakarsh@users.noreply.github.com>
2023-03-26 13:17:51 +0530

    Time: 98 ms (41.18%), Space: 50.5 MB (5.88%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    vector< int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
-
+    vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
     bool primeSubOperation(vector<int>& nums) {
-        vector<int>v = sieve(1000);
-        int prev=0;
-        // for(auto &i:v){
-        //     cout<<i<<" ";
-        // }
-        // cout<<endl;
-        for(int i=0;i<nums.size();i++){
-            int k=-1;
-            int neww=nums[i];
-            for(int j=0;j<v.size();j++){
-                if(v[j]<nums[i] && (nums[i]-v[j])>prev){
-                    neww=nums[i]-v[j];
+        vector<int> sv=sieve(2000);
+        set<int> st(begin(sv),end(sv));
+        for(int i=0;i<nums.size();++i)
+        {
+            if(i==0)
+            {
+                auto it = st.lower_bound(nums[i]);
+                if(*it>=nums[i])
+                {
+                    if(it!=st.begin())
+                    --it;
                 }
+                if(*it<nums[i])
+                nums[i]-=*it;
             }
-            nums[i]=neww;
-            if(prev>=nums[i]){
-                return false;
+            else
+            {
+                int diff = nums[i]-nums[i-1];
+                auto it = st.lower_bound(diff);
+                if(*it>=nums[i]-nums[i-1]) --it;
+                // cout << diff << endl;
+                if(nums[i]-*it>nums[i-1])
+                nums[i]-=*it;
             }
-            prev=nums[i];
-            // cout<<nums[i]<<" "<<prev<<" ";
         }
-        
-        
+        // for(auto &it:nums) cout << it << " ";
+        for(int i=0;i<nums.size()-1;++i)
+        if(nums[i]>=nums[i+1]) return false;
         return true;
     }
 };
