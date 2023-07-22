5a341466d73893d1825cdd5bcd6e02f822b2e071
Nihad Prakarsh <nihadprakarsh@gmail.com>
2023-07-23 01:52:55 +0530

    Time: 487 ms (57.02%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,8 +1,6 @@
-SELECT COALESCE(
-        (select distinct(salary)
-        from employee
-        where salary is not null
-        order by salary DESC
-        limit 1,1
-    ),NULL
-) as SecondHighestSalary;
+with cte as
+    (select *, DENSE_RANK() over(ORDER by salary DESC)
+    as r
+    from employee)
+
+select IFNULL((select salary from cte where r=2 limit 1),null) as SecondHighestSalary;
