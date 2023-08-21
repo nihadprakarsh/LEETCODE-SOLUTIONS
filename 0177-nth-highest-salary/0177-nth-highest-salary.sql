b360acc05e79647d13cb59edb3bfb2ebc5cdfbea
Nihad Prakarsh <nihadprakarsh@gmail.com>
2023-08-21 10:27:56 +0530

    Time: 829 ms (49.27%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -3,9 +3,9 @@ BEGIN
 set N = N-1;
   RETURN (
       # Write your MySQL query statement below.
-      select ifnull((select distinct salary
-      from employee
-      order by salary desc
-      limit 1 offset N),null)
+      select ifnull((select distinct salary 
+        from employee
+        order by salary desc
+        limit 1 offset N),null)
   );
 END
