221eaa8a40d57dde61c242dfbf3b77b766da6e5c
Nihad Prakarsh <nihadprakarsh@gmail.com>
2024-01-08 18:11:36 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (100.00%) - LeetHub

---
+++ 10033-minimum-number-of-operations-to-make-x-and-y-equal/10033-minimum-number-of-operations-to-make-x-and-y-equal.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int dp[10000+1];
+    int find(int x,int y)
+    {
+        if(x<=y) return y-x;
+        int ans=abs(x-y);
+        ans=min(ans,1+x%5+find(x/5,y));
+        ans=min(ans,1+(5-x%5)+find(x/5+1,y));
+        ans=min(ans,1+x%11+find(x/11,y));
+        ans=min(ans,1+(11-x%11)+find(x/11+1,y));
+        return dp[x]=ans;
+
+    }
+    int minimumOperationsToMakeEqual(int x, int y) {
+        memset(dp,-1,sizeof(dp));
+        return find(x,y);
+    }
+};
